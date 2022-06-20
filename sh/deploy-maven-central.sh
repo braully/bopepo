16ef921ced672992a78ad7a9d263ecb35b3d7f22
Braully Rocha <braully@gmail.com>
2022-06-20 16:45:17 -0300

    Liberação versão 1.0.0

--- sh/deploy-maven-central.sh
+++ sh/deploy-maven-central.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 JAVA_TOOL_OPTIONS="-Dhttps.protocols=TLSv1.2 -Djavax.net.ssl.trustStore=cacerts -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=JKS"
-./mvnw -s sh/settings-private.xml -Pdeploy,release clean package -DskipTests=true
-#./mvnw -s sh/settings-private.xml clean package
+./mvnw -s sh/settings-private.xml -Pdeploy,release clean deploy
