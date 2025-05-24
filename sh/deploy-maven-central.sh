cb863f50c625253ce9b792e83b8f512dbc21c5e7
Braully Rocha <braully@gmail.com>
2025-05-24 11:05:27 -0300

    correções na migração do maven central.

--- sh/deploy-maven-central.sh
+++ sh/deploy-maven-central.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 JAVA_TOOL_OPTIONS="-Dhttps.protocols=TLSv1.2 -Djavax.net.ssl.trustStore=cacerts -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStoreType=JKS"
-./mvnw -s sh/settings-private.xml -Pdeploy,release clean deploy
+./mvnw -s sh/settings-private.xml -Possrh,deploy,release clean deploy
